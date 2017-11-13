f032eba71578c8a769f72a190c91b4a8b828eb8c
Arun Sivaraman <arunsivaramanneo@gmail.com>
2017-11-13 14:32:36 +0530

    Initial Commit Trying to Buid a working model

--- Files/main.py
+++ Files/main.py
@@ -23,12 +23,14 @@ class main(Gtk.Window):
         installedPackageDescription = copyContentsFromFile("installedPackageDescription.txt")
         installedPackageName = copyContentsFromFile("installedPackageNames.txt")
 
+        package_count = len(installedPackageName)
         installationCheck = []
         background_color = []
         for i in range(len(installedPackageName)):
             installationCheck.append(True)
             background_color = setBackgroundColor(i)
             installedStore.append([installationCheck[i],installedPackageName[i].strip('\n'),installedPackageDescription[i].strip('\n'),background_color])
+
 
 
 win = main()
@@ -40,11 +42,31 @@ win._getinstalledapplications()
 
 win.connect("delete-event",Gtk.main_quit)
 
+list_box = Gtk.VPaned()
+win.add(list_box)
 win.set_border_width(20)
-vbox = Gtk.VBox(spacing=10)
-win.add(vbox)
+packageframe = Gtk.Frame()
+list_box.set_position(50)
+list_box.add1(packageframe)
+grid = Gtk.Grid()
 frame1 = Gtk.Frame()
-vbox.add(frame1)
+list_box.add2(frame1)
+label = Gtk.Label()
+label.set_text("Available Package Syncs :")
+grid.attach(label,1,1,1,1)
+grid.set_border_width(5)
+grid.set_column_spacing(20)
+packageframe.add(grid)
+sync_store = Gtk.ListStore(str)
+package_combobox = Gtk.ComboBox.new_with_model(sync_store)
+sync_store.append(["Currently Installed Packages"])
+sync_store.append(["Insalled Packages as on : 01-Nov"])
+renderer_combobox = Gtk.CellRendererText()
+package_combobox.pack_start(renderer_combobox,True)
+package_combobox.add_attribute(renderer_combobox,"text",0)
+package_combobox.set_active(0)
+grid.attach_next_to(package_combobox,label,Gtk.PositionType.RIGHT,5,1)
+
 
 for i,column_title in enumerate(Const.COLUMN_HEADER):
     if i == 0:
